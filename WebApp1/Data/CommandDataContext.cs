8a5fdaa5a7dee25d15e7539f792cf465a505af8d
Robin coppens <robincoppens@hotmail.com>
2020-11-10 09:33:08 +0100

    Add project files.

---
+++ WebApp1/Data/CommandDataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp1.Models;
+
+namespace WebApp1.Data
+{
+    public class CommandDataContext : DbContext
+    {
+        public CommandDataContext(DbContextOptions<CommandDataContext> opt ) : base(opt)
+        {
+
+        }
+
+        public DbSet<Command> Commands { get; set; }
+ 
+    }
+}
